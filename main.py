d99cd647c9d095c9c192055ba57dd9718be2b992
YeJin-Choi88 <80390602+YeJin-Choi88@users.noreply.github.com>
2022-11-12 23:40:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import streamlit as st
 import nltk
 import spacy
 from PIL import Image
-nltk.download('punkt')
+
+nltk.download('averaged_perceptron_tagger')
 nlp = spacy.load('en_core_web_sm')
 
 
