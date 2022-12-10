93423d805b7a3a576421200143a09043a1c4b820
YeJin-Choi88 <80390602+YeJin-Choi88@users.noreply.github.com>
2022-12-10 17:07:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -112,7 +112,6 @@ def image_view(sentence):
 
 def render_svg(svg):
     """Renders the given svg string."""
-    resize_svg(svg)
     b64 = base64.b64encode(svg.encode('utf-8')).decode("utf-8")
     html = r'<img src="data:image/svg+xml;base64,%s"/>' % b64
     st.write(html, unsafe_allow_html=True)
